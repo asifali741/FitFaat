34183f086b176cda7528b1dd145fc1e428481eb4
Asif Ali <221866@students.au.edu.pk>
2025-08-11 11:30:56 +0500

    Diet Screen File Added

---
+++ app/DietPlanScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function DietPlanScreen() {
+  return (
+    <View>
+      <Text>DietPlanScreen</Text>
+    </View>
+  )
+}
