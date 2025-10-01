23ef4d24a2040cead6427de8317abec84727587f
Shayan <shayandp75@gmail.com>
2025-10-01 14:06:37 +0500

    Added better comments, optimized logic for svg animation, added view button

--- app/(main)/(dashboard)/Day.tsx
+++ app/(main)/(dashboard)/Day.tsx
@@ -14,20 +14,6 @@ const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 //const finalProgress = 55; // percent
 const radius = 30;
 const circumference = 2 * Math.PI * radius;
-/**
- const Day_Day = {
-   dayNo: 1,
-   date: "2023-10-01",
-   achievedCalories : 1500,
-   achieviedHydration: 2000,
-   targetCalories: 2000,
-   targetHydration: 2000,
-   remarks: "Felt good today!",
-   duration: 60, // in minutes
- }
- * 
- */
-
 
 export const Days = ({props, onDayPress}: {props: Day, onDayPress: (dayNo : number) => void}) => {
   if(props.status === 'locked'){ //props false = locked day
@@ -43,10 +29,9 @@ export const Days = ({props, onDayPress}: {props: Day, onDayPress: (dayNo : numb
 
 const ActiveDay = ({info, Press}: {info: Day, Press: (dayNo : number) => void}) => {
   const progress = useSharedValue(0);
-  const finalProgress : number = Math.min(100, Math.round((info.achievedCalories / info.targetCalories) * 100));
+  const finalProgress : number = Math.min(100, Math.round(((info.achievedCalories + info.achieviedHydration) / (info.targetCalories + info.targetHydration)) * 100));
   var time = '00:00:00'
-  const formatDuration = (minutes: number): string => {
-      const totalSeconds = minutes * 60;
+  const formatDuration = (totalSeconds: number): string => {
       const hours = Math.floor(totalSeconds / 3600);
       const mins = Math.floor((totalSeconds % 3600) / 60);
       const secs = totalSeconds % 60;
@@ -63,7 +48,7 @@ if (info.duration != null && typeof info.duration === "number") {
   
   useEffect(() => {
     progress.value = withTiming(finalProgress, { duration: 1000 });
-  }, []);
+  }, [finalProgress]);
   
   // Animated strokeDashoffset
   const animatedprops = useAnimatedProps(() => {
@@ -75,64 +60,64 @@ if (info.duration != null && typeof info.duration === "number") {
   });
 
   return (
-    <View style={styles.activeItem}>
-    {/* Progress Circle */}
-    <View style={styles.topHeader}>
-      <View style={styles.circleActiveWrapper}>
-        <Svg
-          height={radius * 2 + 20} 
-          width={radius * 2 + 20}
-          viewBox={`0 0 ${radius * 2 + 20} ${radius * 2 + 20}`}
-          >
-          {/* Background Circle */}
-          <Circle
-            cx="50%"
-            cy={radius + 10}
-            r={radius}
-            stroke="#808080"
-            strokeWidth={8}
-            strokeDasharray={circumference}
-            fill="none"
-            />
-          {/* Progress Circle */}
-          <AnimatedCircle
-            cx="50%"
-            cy={radius + 10}
-            r={radius}
-            stroke="#12b82eff"
-            strokeWidth={5}
-            strokeDasharray={circumference}
-            animatedProps={animatedprops}
-            strokeLinecap="round"
-            fill="none"
-            />
-          <SvgText
-            x="50%"
-            y="50%"
-            textAnchor="middle"
-            dy=".3em"
-            fontSize="16"
-            fontWeight="bold"
-            fill="#000"
-            fontStyle="italic"
+    <View style={styles.activeItem /**Columnize the box */}>
+    <View style={styles.topHeader  /**Row of Circle, DayDate, Button */}>
+      {/* Progress Circle */}
+        <View style={styles.circleActiveWrapper /**Circle */}>
+          <Svg
+            height={radius * 2 + 20} 
+            width={radius * 2 + 20}
+            viewBox={`0 0 ${radius * 2 + 20} ${radius * 2 + 20}`}
             >
-            {finalProgress}%
-          </SvgText>
-        </Svg>
-      </View>
-
-      <View style={styles.activeContentWrapper}>
-        <View style={styles.row}>
-          <Text style={styles.activeDayText}>Day: 0{info.dayNo} </Text> 
-          <Text style={styles.activeDateText}>{info.date}</Text>
+            {/* Background Circle */}
+            <Circle
+              cx="50%"
+              cy={radius + 10}
+              r={radius}
+              stroke="#808080"
+              strokeWidth={8}
+              strokeDasharray={circumference}
+              fill="none"
+              />
+            {/* Progress Circle */}
+            <AnimatedCircle
+              cx="50%"
+              cy={radius + 10}
+              r={radius}
+              stroke="#00FF44"
+              strokeWidth={8}
+              strokeDasharray={circumference}
+              animatedProps={animatedprops}
+              strokeLinecap="round"
+              fill="none"
+              />
+            <SvgText
+              x="50%"
+              y="50%"
+              textAnchor="middle"
+              dy=".3em"
+              fontSize="16"
+              fontWeight="bold"
+              fill="#000"
+              fontStyle="italic"
+              >
+              {finalProgress}%
+            </SvgText>
+          </Svg>
         </View>
-        <Text style={styles.activeSubtitle}>{info.remarks}</Text>
-        {/* You can map your details here */}
-      </View>
-
-      <Pressable onPress={() => Press(info.dayNo)} style={{backgroundColor: '#000000', height: 30, width: 70, borderRadius: 8, justifyContent: 'center', alignItems: 'center'}} >
-        <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>View</Text>
-        </Pressable>
+        {/* Day & Date & Remark */}
+        <View style={styles.activeContentWrapper /**DayDate */}>
+          <View style={styles.row}>
+            <Text style={styles.activeDayText}>Day: 0{info.dayNo} </Text> 
+            <Text style={styles.activeDateText}>{info.date}</Text>
+          </View>
+          <Text style={styles.activeSubtitle}>{info.remarks}</Text>
+        </View>
+        {/* View Button */}
+        <Pressable onPress={() => Press(info.dayNo)} 
+                  style={{backgroundColor: '#000000', height: '35%', width: '20%', borderRadius: 8, justifyContent: 'center', alignItems: 'center', marginTop: '5%'}} >
+          <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>View</Text>
+          </Pressable>
     </View>
     <View style={styles.bottomBody}>
       <View style={styles.activeInformation}>
@@ -179,34 +164,19 @@ const FinishedDay = ({info, Press}: {info: Day, Press: (dayNo : number) => void}
           cx="50%"
           cy="50%"
           r={radius}
-          stroke="#4c4d8bff"
+          stroke="#808080"
           strokeWidth={8}
           strokeDasharray={circumference}
           fill="none"
         />
-        {/* Progress Circle */}
-        <AnimatedCircle
-          cx="50%"
-          cy="50%"
-          r={radius}
-          stroke="#12b82eff"
-          strokeWidth={5}
-          strokeDasharray={circumference}
-          strokeLinecap="round"
-          fill="none"
-        />
-        <SvgText
-          x="50%"
-          y="50%"
-          textAnchor="middle"
-          dy=".3em"
-          fontSize="16"
-          fontWeight="bold"
-          fill="#000"
-          fontStyle="italic"
-        >
-          Done
-        </SvgText>
+          <SvgImage
+            x={(radius * 2 + 20)/2 - 20}
+            y={(radius * 2 + 20)/2 - 20}
+            width={(radius * 2 + 20) -40}
+            href={require("../../../assets/images/Finished.png")}  // Local image
+            preserveAspectRatio="xMidYMid slice"
+          />
+        <Circle/>
       </Svg>
     </View>
 
@@ -216,7 +186,10 @@ const FinishedDay = ({info, Press}: {info: Day, Press: (dayNo : number) => void}
         <Text style={styles.dateText}>{info.date}</Text>
       </View>
       <Text style={styles.subtitle}>Goal: {finalProgress}%</Text>
-      {/* You can map your details here */}
+      
+      <Pressable onPress={() => Press(info.dayNo)} style={{backgroundColor: '#000000', height: 30, width: 70, borderRadius: 8, justifyContent: 'center', alignItems: 'center'}} >
+        <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>View</Text>
+      </Pressable>
     </View>
   </View>
   );
@@ -265,47 +238,34 @@ const LockedDay = ({info} : {info: Day}) => {
 }
 
 const styles = StyleSheet.create({
-  list: {
-    height: "90%",
-    width: "90%",
-    borderWidth: 2,
-    marginTop: 10,
-    margin: "5%",
-    borderRadius: 12,
-    borderColor: "#b69a9aff",
-    backgroundColor: "#EDCCC2",
-  },
   listitem: {
     margin: "2%",
     height: "9.5%",
     flexDirection: "row",      
     alignItems: "center",      
-    backgroundColor: '#EDCCC2',
-    //backgroundColor: "#fff",
+    backgroundColor: '#EDCCC2',   //ui_elements screenColor
     borderRadius: 12,
     padding: 10,
     marginBottom: 10,
     shadowColor: "#000",
-    shadowOpacity: 0.4,
+    shadowOpacity: 0.2,
     shadowRadius: 10,
-    elevation: 21,
-
+    elevation: 15,
   },
+
   activeItem: {
     margin: "2%",
     height: "30%",
     flexDirection: "column",      
     //alignItems: "center",      
-    backgroundColor: '#EDCCC2',
-    //backgroundColor: "#fff",
+    backgroundColor: '#EDCCC2',  //ui_elements screenColor
     borderRadius: 14,
     padding: 10,
     marginBottom: 10,
     shadowColor: "#7c1515ff",
-    shadowOpacity: 0.4,
+    shadowOpacity: 0.3,
     shadowRadius: 10,
     elevation: 21,
-
   },
   row: {
     flexDirection: "row",
@@ -326,7 +286,6 @@ const styles = StyleSheet.create({
   contentWrapper: {
     flex: 1,
     paddingLeft: 10,
-    
   },
   activeContentWrapper: {
     flex: 1,
