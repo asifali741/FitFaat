89a19d3a3e33a048d8298222b9fcedfb9f548b6d
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2025-11-17 20:41:06 +0500

    main screen improved

--- components/getStarted.js
+++ components/getStarted.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
+    heightPercentageToDP as hp,
+    widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
 
 export const getStarted = StyleSheet.create({
