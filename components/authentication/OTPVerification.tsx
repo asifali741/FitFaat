48514cb1cef0409f04aa0e4fa7fad696fd946adc
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2025-11-18 01:46:05 +0500

    fixed issue of ref

--- components/authentication/OTPVerification.tsx
+++ components/authentication/OTPVerification.tsx
@@ -164,7 +164,7 @@ export default function OTPVerification() {
             {otp.map((digit, index) => (
               <TextInput
                 key={index}
-                ref={(ref) => (inputRefs.current[index] = ref)}
+                ref={(ref) => { inputRefs.current[index] = ref; }}
                 style={[
                   styles.otpInput,
                   digit && styles.otpInputFilled
