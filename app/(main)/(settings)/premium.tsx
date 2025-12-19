5959a07ee7f3047fe64673ca3c6c7127be0a44f1
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2025-12-20 01:54:10 +0500

    issue

--- app/(main)/(settings)/premium.tsx
+++ app/(main)/(settings)/premium.tsx
@@ -8,18 +8,18 @@ import Constants from "expo-constants";
 import { useRouter } from "expo-router";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
-    ActivityIndicator,
-    Alert,
-    Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
+  ActivityIndicator,
+  Alert,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import {
-    heightPercentageToDP as hp,
-    widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
 import { SafeAreaView } from "react-native-safe-area-context";
 
